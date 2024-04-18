df8454e01e0bdf33f22334bdb92a0281f1ba4538
Tomoya Honda <tomoya.honda1219@gmail.com>
2024-04-18 13:03:05 +0000

    Remove unnecessary if statement & simplify list initialization

--- problems/medium/python3/322.coin-change.step3.py
+++ problems/medium/python3/322.coin-change.step3.py
@@ -8,12 +8,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         inf = 10**9
-        min_coins = [inf for _ in range(amount + 1)]
+        min_coins = [inf] * (amount + 1)
         min_coins[0] = 0
         for coin in coins:
             for i in range(coin, amount + 1):
-                if min_coins[i - coin] != inf:
-                    min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
         return min_coins[amount] if min_coins[amount] != inf else -1
 # @lc code=end
 
