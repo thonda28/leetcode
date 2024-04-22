e6f0c5b7b749802a4d9581ffe5ae18d4c03b6edf
Tomoya Honda <tomoya.honda1219@gmail.com>
2024-04-22 15:43:22 +0000

    Modify variable names

--- problems/medium/python3/207/course-schedule.step2.py
+++ problems/medium/python3/207/course-schedule.step2.py
@@ -8,24 +8,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         postrequisites_of_courses = [[] for _ in range(numCourses)]
-        indegree_of_courses = [0 for _ in range(numCourses)]
+        num_prerequisites_of_courses = [0 for _ in range(numCourses)]
 
         for course, prerequisite in prerequisites:
             postrequisites_of_courses[prerequisite].append(course)
-            indegree_of_courses[course] += 1
+            num_prerequisites_of_courses[course] += 1
 
-        non_indegree_courses = []
+        independent_courses = []
         for i in range(numCourses):
-            if indegree_of_courses[i] == 0:
-                non_indegree_courses.append(i)
+            if num_prerequisites_of_courses[i] == 0:
+                independent_courses.append(i)
 
         num_of_processed_courses = 0
-        while non_indegree_courses:
-            nic = non_indegree_courses.pop()
-            for postrequisites in postrequisites_of_courses[nic]:
-                indegree_of_courses[postrequisites] -= 1
-                if indegree_of_courses[postrequisites] == 0:
-                    non_indegree_courses.append(postrequisites)
+        while independent_courses:
+            ic = independent_courses.pop()
+            for postrequisites in postrequisites_of_courses[ic]:
+                num_prerequisites_of_courses[postrequisites] -= 1
+                if num_prerequisites_of_courses[postrequisites] == 0:
+                    independent_courses.append(postrequisites)
             num_of_processed_courses += 1
         return num_of_processed_courses == numCourses
 # @lc code=end
