935dd2cb9fc34d0cd367731cf5bf987b97fb09b0
Tomoya Honda <tomoya.honda1219@gmail.com>
2024-04-27 03:05:27 +0000

    Rename variable name

--- problems/medium/python3/3/longest-substring-without-repeating-characters.step3.py
+++ problems/medium/python3/3/longest-substring-without-repeating-characters.step3.py
@@ -7,13 +7,13 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_index_map = dict()
+        char_to_index = dict()
         start = 0
         longest_length = 0
         for i, char in enumerate(s):
-            if char in char_index_map:
-                start = max(start, char_index_map[char] + 1)
-            char_index_map[char] = i
+            if char in char_to_index:
+                start = max(start, char_to_index[char] + 1)
+            char_to_index[char] = i
             longest_length = max(longest_length, i - start + 1)
         return longest_length
 # @lc code=end
