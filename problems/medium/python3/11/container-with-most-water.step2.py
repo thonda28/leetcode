267291757ea9452b14c421dbf8ca6b23c1919a46
Tomoya Honda <tomoya.honda1219@gmail.com>
2024-07-27 19:34:23 +0000

    Modify step2

--- problems/medium/python3/11/container-with-most-water.step2.py
+++ problems/medium/python3/11/container-with-most-water.step2.py
@@ -5,35 +5,72 @@
 #
 
 # @lc code=start
-class RangeMaximumQuerySegmentTree:
-    def __init__(self, nums: List[int], min_value: int):
-        self.min_value = min_value
+from typing import Callable, List
+
+
+class SegmentTree:
+    def __init__(self, nums: List[int], func: Callable[[int, int], int], default_value: int):
+        self.func = func
+        self.default_value = default_value
         x = 1
         while x < len(nums):
             x *= 2
         self.n = x
-        self.tree = [self.min_value] * (2 * self.n - 1)
+        self.tree = [self.default_value] * (2 * self.n - 1)
 
-        for index, num in enumerate(nums):
-            self.update(index, num)
+        for index in range(len(nums)):
+            self.update(index, nums[index])
 
     def update(self, index: int, value: int):
         index += self.n - 1
         self.tree[index] = value
         while index > 0:
             index = (index - 1) // 2
-            self.tree[index] = max(self.tree[2 * index + 1], self.tree[2 * index + 2])
+            self.tree[index] = self.func(self.tree[2 * index + 1], self.tree[2 * index + 2])
 
-    def range_maximum_query(self, begin: int, end: int) -> int:
+    def range_query(self, begin: int, end: int) -> int:
         return self.__sub_query(begin, end, 0, 0, self.n)
 
     def __sub_query(self, begin: int, end: int, index: int, left: int, right: int) -> int:
         if end <= left or right <= begin:
-            return self.min_value
+            return self.default_value
         if begin <= left and right <= end:
             return self.tree[index]
         mid = (left + right) // 2
         val1 = self.__sub_query(begin, end, index * 2 + 1, left, mid)
         val2 = self.__sub_query(begin, end, index * 2 + 2, mid, right)
-        return max(val1, val2)
+        return self.func(val1, val2)
+
+
+class Solution:
+    def swap_index_value(self, nums: List[int], use_last_seen: bool, default_value: int) -> List[int]:
+        n = len(nums)
+        swapped_list = [default_value] * (max(nums) + 1)
+        start, stop, step = (0, n, 1) if use_last_seen else (n - 1, -1, -1)
+
+        for index in range(start, stop, step):
+            swapped_list[nums[index]] = index
+
+        return swapped_list
+
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        max_height = max(height)
+        max_amount = 0
+
+        last_seen_map = self.swap_index_value(height, use_last_seen=True, default_value=-1)
+        max_seg_tree = SegmentTree(last_seen_map, func=max, default_value=-1)
+        for left in range(n):
+            left_height = height[left]
+            right = max_seg_tree.range_query(left_height, max_height + 1)
+            max_amount = max(max_amount, left_height * (right - left))
+
+        first_seen_map = self.swap_index_value(height, use_last_seen=False, default_value=max_height + 1)
+        min_seg_tree = SegmentTree(first_seen_map, func=min, default_value=len(height) + 1)
+        for right in range(n - 1, -1, -1):
+            right_height = height[right]
+            left = min_seg_tree.range_query(right_height, max_height + 1)
+            max_amount = max(max_amount, right_height * (right - left))
+
+        return max_amount
 # @lc code=end
